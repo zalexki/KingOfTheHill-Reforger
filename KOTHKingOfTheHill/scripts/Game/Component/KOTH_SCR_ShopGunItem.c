f51ddd8a2470066e959a3e13670dc36949a431cb
zalexki <haumalex@gmail.com>
2023-08-20 00:48:27 +0200

    wip

---
+++ KOTHKingOfTheHill/scripts/Game/Component/KOTH_SCR_ShopGunItem.c
@@ -0,0 +1,10 @@
+[BaseContainerProps(configRoot:true)]
+class KOTH_SCR_ShopGunItem
+{
+	protected ref Resource m_ItemResource;
+	protected ref Resource m_MagazineResource;
+	//protected ref Resource m_SilencerNumber;
+	protected int m_MagazineNumber;
+	protected int m_Price;
+	protected int m_Level;
+}
