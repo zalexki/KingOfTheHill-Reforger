0a1460a9acde9446aef2881510d880577568e506
Aqkwa_ <zaiceofficiel@hotmail.com>
2023-08-20 00:44:55 +0200

    layout shop and base script for layout shop

--- KOTHKingOfTheHill/scripts/Game/UI/Menu/KOTH_ShopGunClass.c
+++ KOTHKingOfTheHill/scripts/Game/UI/Menu/KOTH_ShopGunClass.c
@@ -9,24 +9,39 @@ class KOTH_ShopGunClass : ChimeraMenuBase {
 		m_wRoot = GetRootWidget();
 		SCR_NavigationButtonComponent cancel = SCR_NavigationButtonComponent.GetNavigationButtonComponent("Cancel", m_wRoot);
 		if (cancel)
+		{
 			cancel.m_OnActivated.Insert(OnClickEscape);
+		}
 		
-		SCR_NavigationButtonComponent buy = SCR_NavigationButtonComponent.GetNavigationButtonComponent("PurchaseOnceButton", m_wRoot);
+		
+		SCR_ButtonBaseComponent buy = SCR_ButtonBaseComponent.GetButtonBase("PurchaseOnceButton", m_wRoot);
 		if (buy)
-			buy.m_OnActivated.Insert(OnClickBuy);
+		{
+			buy.m_OnClicked.Insert(OnClickBuy);
+		}	
+	
+
+		ItemPreviewManagerEntity m_PreviewManager = GetGame().GetItemPreviewManager();
+		
+		ItemPreviewWidget wRenderTarget = ItemPreviewWidget.Cast(m_wRoot.FindAnyWidget("Preview_Image"));
+		if (wRenderTarget && m_PreviewManager)
+		{			
+			m_PreviewManager.SetPreviewItemFromPrefab(wRenderTarget, "{FA5C25BF66A53DCF}Prefabs/Weapons/Rifles/AK74/Rifle_AK74.et", null, true);
+		}
+		
 	}
 	
 	protected void OnClickBuy() 
 	{
 		PlayerManager playerManager = GetGame().GetPlayerManager();
 		PlayerController controller = GetGame().GetPlayerController();
-		string playerName = playerManager.GetPlayerName(playerId);
+		//string playerName = playerManager.GetPlayerName(playerId);
 		
 		SCR_BaseGameMode gameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
 		KOTH_ScoringGameModeComponent m_scoreComp = KOTH_ScoringGameModeComponent.Cast(gameMode.FindComponent(KOTH_ScoringGameModeComponent));
 		
 		int amount = 3;
-		m_scoreComp.DoRpcBuy(amount, playerName);
+		//m_scoreComp.DoRpcBuy(amount, playerName);
 		
 		HUD_NotifBuy(amount);
 	}
