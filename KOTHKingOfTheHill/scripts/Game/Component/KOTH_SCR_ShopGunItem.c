24cf8fc2f9e559544b90a5cca10cdc695186cd94
zalexki <haumalex@gmail.com>
2023-08-20 04:04:54 +0200

    plugit

--- KOTHKingOfTheHill/scripts/Game/Component/KOTH_SCR_ShopGunItem.c
+++ KOTHKingOfTheHill/scripts/Game/Component/KOTH_SCR_ShopGunItem.c
@@ -1,10 +1,32 @@
 [BaseContainerProps(configRoot:true)]
 class KOTH_SCR_ShopGunItem
 {
-	protected ref Resource m_ItemResource;
-	protected ref Resource m_MagazineResource;
+	[Attribute(desc: "Prefab of the Arsenal item", params: "et")]
+	ResourceName m_itemResource;
+	
+	[Attribute(desc: "Prefab of the magazine", params: "et")]
+	ResourceName m_magazineResource;
+	
+	[Attribute(desc: "Prefab of the secondary magazine", params: "et")]
+	ResourceName m_secondaryMagazineResource;
+	
+	[Attribute("5", desc: "Number of magazines added when equiped")]
+	int m_magazineNumber;
+	
+	[Attribute("1000", desc: "Price buy once")]
+	int m_priceOnce;
+	
+	[Attribute("100000", desc: "Price buy permanent")]
+	int m_pricePermanent;
+	
+	[Attribute("5", desc: "Level required to buy the item")]
+	int m_level;
+	
+	[Attribute("GUN", desc: "GUN, SCOPE, VEHICLE")]
+	string m_category;
+	
+	[Attribute("bigGun", desc: "name")]
+	string m_itemName;
+	
 	//protected ref Resource m_SilencerNumber;
-	protected int m_MagazineNumber;
-	protected int m_Price;
-	protected int m_Level;
 }
--- KOTHKingOfTheHill/scripts/Game/UI/Menu/KOTH_ShopGunClass.c
+++ KOTHKingOfTheHill/scripts/Game/UI/Menu/KOTH_ShopGunClass.c
@@ -2,6 +2,13 @@ class KOTH_ShopGunClass : ChimeraMenuBase
 {
 	protected Widget m_wRoot;  
 	protected SCR_PlayerController m_playerController;
+	protected ItemPreviewManagerEntity m_PreviewManager;
+	
+	override void OnMenuInit()
+	{
+		super.OnMenuInit();
+		m_PreviewManager = GetGame().GetItemPreviewManager();
+	}
 	
 	override void OnMenuOpen()
 	{
@@ -14,37 +21,81 @@ class KOTH_ShopGunClass : ChimeraMenuBase
 			cancel.m_OnActivated.Insert(OnClickEscape);
 		}
 
-		VerticalLayoutWidget contentContainer = VerticalLayoutWidget.Cast(m_wRoot.FindAnyWidget("ContentContainer"));
-		Widget w = GetGame().GetWorkspace().CreateWidgets("{20EF71DE68A5887E}UI/Layouts/HUD/Shop/ItemListUILayout.layout", contentContainer);
-		
-		SCR_ButtonBaseComponent buy = SCR_ButtonBaseComponent.GetButtonBase("PurchaseOnceButton", m_wRoot);
-		if (buy)
-		{
-			buy.m_OnClicked.Insert(OnClickBuy);
-		}
-		
-		ItemPreviewManagerEntity m_PreviewManager = GetGame().GetItemPreviewManager();
-		
-		ItemPreviewWidget wRenderTarget = ItemPreviewWidget.Cast(m_wRoot.FindAnyWidget("Preview_Image"));
-		if (wRenderTarget && m_PreviewManager)
-		{			
-			m_PreviewManager.SetPreviewItemFromPrefab(wRenderTarget, "{FA5C25BF66A53DCF}Prefabs/Weapons/Rifles/AK74/Rifle_AK74.et", null, true);
-		}
+		AddItemsFromConfig();
 	}
 	
-	protected void OnClickBuy() 
+	protected void AddItemsFromConfig()
 	{
+		KOTH_SCR_ShopGunItemList itemList = SCR_ConfigHelperT<KOTH_SCR_ShopGunItemList>.GetConfigObject("{232D181B9F9FE8D1}Configs/ShopGunItemList.conf");
+		VerticalLayoutWidget contentContainer = VerticalLayoutWidget.Cast(m_wRoot.FindAnyWidget("ContentContainer"));
+		
+		foreach (KOTH_SCR_ShopGunItem item : itemList.GetItems())
+		{
+			Log("add item to shop " + item.m_itemName);
+			Widget newRow = GetGame().GetWorkspace().CreateWidgets("{20EF71DE68A5887E}UI/Layouts/HUD/Shop/ItemListUILayout.layout", contentContainer);
+			
+			// add click events
+			SCR_ButtonBaseComponent buyOnce = SCR_ButtonBaseComponent.GetButtonBase("PurchaseOnceButton", newRow);
+			if (buyOnce)
+				buyOnce.m_OnClicked.Insert(OnClickBuyOnce);
+
+			
+			SCR_ButtonBaseComponent buyPermanent = SCR_ButtonBaseComponent.GetButtonBase("PurchasePermanentButton", newRow);
+			if (buyPermanent)
+				buyPermanent.m_OnClicked.Insert(OnClickBuyPermanent);
+
+			// set text infos
+			TextWidget nameWidget = TextWidget.Cast(newRow.FindAnyWidget("ItemName"));
+			nameWidget.SetText(item.m_itemName);
+			TextWidget priceOnceWidget = TextWidget.Cast(newRow.FindAnyWidget("PriceOnceText"));
+			priceOnceWidget.SetText(item.m_priceOnce.ToString());
+			TextWidget pricePermWidget = TextWidget.Cast(newRow.FindAnyWidget("PricePermText"));
+			pricePermWidget.SetText(item.m_pricePermanent.ToString());
+			
+			// add item preview
+			ItemPreviewWidget wRenderTarget = ItemPreviewWidget.Cast(newRow.FindAnyWidget("Preview_Image"));
+			if (wRenderTarget && m_PreviewManager)
+			{
+				Log(item.m_itemResource.GetPath());
+				m_PreviewManager.SetPreviewItemFromPrefab(wRenderTarget, item.m_itemResource, null, false);
+			}
+		}
+	}
+ 	
+	// TODO: equip weapon,magazines ...
+	protected void OnClickBuyOnce(SCR_ButtonBaseComponent button) 
+	{
+		Widget row = button.GetRootWidget().GetParent().GetParent();
+		TextWidget priceOnceWidget = TextWidget.Cast(row.FindAnyWidget("PriceOnceText"));
+
 		PlayerManager playerManager = GetGame().GetPlayerManager();
 		PlayerController controller = GetGame().GetPlayerController();
 		int playerId = controller.GetPlayerId();
 		string playerName = playerManager.GetPlayerName(playerId);
 
 		IEntity cont = controller.GetControlledEntity();
-		Print(cont);
 		KOTH_SCR_PlayerProfileComponent kothPlayerComp = KOTH_SCR_PlayerProfileComponent.Cast(controller.FindComponent(KOTH_SCR_PlayerProfileComponent));
-		Print(kothPlayerComp);
+		
+		int amount = priceOnceWidget.GetText().ToInt();
+		kothPlayerComp.DoAskRpcBuy(amount, playerName);
+		
+		HUD_NotifBuy(amount);
+	}
+	
+	protected void OnClickBuyPermanent(SCR_ButtonBaseComponent button) 
+	{
+		Widget row = button.GetRootWidget().GetParent().GetParent();
+		TextWidget priceOnceWidget = TextWidget.Cast(row.FindAnyWidget("PricePermText"));
 
-		int amount = 3;
+		PlayerManager playerManager = GetGame().GetPlayerManager();
+		PlayerController controller = GetGame().GetPlayerController();
+		int playerId = controller.GetPlayerId();
+		string playerName = playerManager.GetPlayerName(playerId);
+
+		IEntity cont = controller.GetControlledEntity();
+		KOTH_SCR_PlayerProfileComponent kothPlayerComp = KOTH_SCR_PlayerProfileComponent.Cast(controller.FindComponent(KOTH_SCR_PlayerProfileComponent));
+		
+		int amount = priceOnceWidget.GetText().ToInt();
 		kothPlayerComp.DoAskRpcBuy(amount, playerName);
 		
 		HUD_NotifBuy(amount);
