23ee82bcc789a689a1911babe63ab07e8b3a87e2
zalexki <haumalex@gmail.com>
2023-09-29 23:28:09 +0200

    badly fix endscreen

---
+++ KOTHKingOfTheHill/scripts/Game/KOTH/GameOverScreen/KOTH_SCR_GameOverScreenManagerComponent.c
@@ -0,0 +1,16 @@
+modded class SCR_BaseGameOverScreenInfo
+{
+//	[Attribute("{9C2D5F0B4AFC2848}UI/Layouts/GameOverEndScreen/EndScreen_KOTH.layout", params: "layout")]
+//	protected ResourceName m_sGameOverScreenBasePrefab;
+	
+//	override void OnPostInit(IEntity owner)
+//	{	
+//		m_sGameOverScreenBasePrefab = ;
+//	}
+	
+//	override ResourceName GetGameOverContentLayout()
+//	{
+//		return "{9C2D5F0B4AFC2848}UI/Layouts/GameOverEndScreen/EndScreen_KOTH.layout";
+//	}
+}
+
